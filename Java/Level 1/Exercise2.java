c996dc1275f194f17672e13dbf6fb0a92801e081
42iscool42 <42iscool42@gmail.com>
2015-10-11 23:28:42 -0400

    Bug Fixes

--- Java/Level 1/Exercise2.java
+++
@@ -1,8 +0,0 @@
-class Exercise2 {
-	public static void main(String[] args) {
-		int five = 3;
-		if (five < 5) {
-			System.out.println("Five is less than 5");
-		}
-	}
-}
--- Java/Level 1/Exercise3.java
+++
@@ -1,8 +0,0 @@
-class Exercise3 {
-	public static void main(String[] args) {
-		String pineapple = "strawberries";
-		if (!pineapple.equals("apples")){
-			System.out.println(pineapple + " is not apples!");
-		}
-	}
-}
