3676c8925c6d5ba66a50029e6fa0b8323d30c027
42iscool42 <42iscool42@gmail.com>
2015-10-14 17:22:19 -0400

    Updating some stuff

---
+++ Java/Level 6/Exercise 1/Employee.java
@@ -0,0 +1,38 @@
+class Employee {
+	private String firstName;
+	private String lastName;
+	private int years;
+	private int salary;
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setLastName(String lastName) {
+		lastName = newName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setYears(int years) {
+		this.years = years;
+	}
+
+	public int getYears() {
+		return years;
+	}
+}
